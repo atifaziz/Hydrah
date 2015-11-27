5affa23645af61dfc401bb5424f0cc61a4d30ee4
Atif Aziz <code@raboof.com>
2015-11-27 19:12:12 +0100

    Build script

---
+++ build.cmd
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+pushd "%~dp0"
+nuget restore && call :build Debug && call :build Release
+popd
+goto :EOF
+
+:build
+if "%PROCESSOR_ARCHITECTURE%"=="x86" set MSBUILD=%ProgramFiles%
+if defined ProgramFiles(x86) set MSBUILD=%ProgramFiles(x86)%
+set MSBUILD=%MSBUILD%\MSBuild\14.0\bin\msbuild
+if not exist "%MSBUILD%" (
+    echo Microsoft Build Tools 2015 does not appear to be installed on this
+    echo machine, which is required to build the solution. You can install
+    echo it from the URL below and then try building again:
+    echo https://www.microsoft.com/en-us/download/detailscd .aspx?id=48159
+    exit /b 1
+)
+"%MSBUILD%" /p:Configuration=%1 /v:m %2 %3 %4 %5 %6 %7 %8 %9
+goto :EOF
